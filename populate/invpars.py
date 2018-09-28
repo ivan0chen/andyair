625e77fcc868f302f7fd6f7859ffd5949c871ea5
ivan0chen <youngfu.chen@gmail.com>
2018-09-28 11:22:07 +0800

    Initial

--- populate/invpars.py
+++ populate/invpars.py
@@ -1,13 +1,16 @@
 
 from populate import base
 from invpars.models import Invpars
+from account.models import User
 
 def populate():
     print('Creating Invoice parameters ... ', end='')
+    user = User.objects.first()
     Invpars.objects.all().delete()
     Invpars.objects.create(midno='81046553', mname='安抵航空貨運代理有限公司', taxno='81046553',
                            seqyy=0, seq01=0, seq02=0, seq03=0, seq04=0, seq05=0, seq06=0,
-                           seq07=0, seq08=0, seq09=0, seq10=0, seq11=0, seq12=0)
+                           seq07=0, seq08=0, seq09=0, seq10=0, seq11=0, seq12=0,
+                           created_by_id=user.id, last_updated_by_id=user.id)
     print('done')
 
 if __name__ == '__main__':
