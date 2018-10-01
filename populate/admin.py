74dc6767a54fa92019ba0a8d6df7f07aa5e9dc93
ivan0chen <youngfu.chen@gmail.com>
2018-10-01 11:31:07 +0800

    Initial

--- populate/admin.py
+++ populate/admin.py
@@ -4,6 +4,7 @@ from account.models import User
 
 def populate():
     print('Creating admin account ... ', end='')
+    User.objects.all().delete()
     User.objects.create_superuser(username='admin', password='admin', email=None)
     print('done')
 
