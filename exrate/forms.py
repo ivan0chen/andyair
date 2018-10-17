2c0e13a77b142cf039693ad48c9124ec1d5adc07
ivan0chen <youngfu.chen@gmail.com>
2018-10-17 09:27:50 +0800

    Initial

--- exrate/forms.py
+++ exrate/forms.py
@@ -2,7 +2,16 @@ from django import forms
 from exrate.models import Exrate
 
 class ExrateForm(forms.ModelForm):
-    code = forms.CharField(label='CODE', min_length=3, max_length=3, required=True, widget=forms.TextInput(attrs={'class':'form-control'}))
+    CHOICES = (
+    ('EUR', 'EUR'), ('USD', 'USD'), ('TWD', 'TWD'), ('AUD', 'AUD'), ('ATS', 'ATS'), ('BEF', 'BEF'), ('CAD', 'CAD'),
+    ('DEM', 'DEM'),
+    ('FRF', 'FRF'), ('HKD', 'HKD'), ('NLG', 'NLG'), ('GBP', 'GBP'), ('SGD', 'SGD'), ('ZAR', 'ZAR'), ('SEK', 'SEK'),
+    ('CHF', 'CHF'),
+    ('JPY', 'JPY'), ('MYR', 'MYR'), ('ITL', 'ITL'), ('XEU', 'XEU'), ('ESP', 'ESP'), ('DKK', 'DKK'), ('FIM', 'FIM'),
+    ('IDR', 'IDR'),
+    ('NZD', 'NZD'), ('NOK', 'NOK'), ('PHP', 'PHP'), ('KRW', 'KRW'), ('THB', 'THB'), ('RMB', 'RMB'))
+
+    code = forms.CharField(label='CODE', min_length=3, max_length=3, required=True, widget=forms.Select(choices=CHOICES, attrs={'class':'form-control'}))
     currency = forms.CharField(label='外幣名稱', max_length=25, required=True, widget=forms.TextInput(attrs={'class':'form-control'}))
     ex_irate = forms.DecimalField(label='買入', max_digits=9, decimal_places=5, widget=forms.NumberInput(attrs={'class':'form-control'}))
     ex_orate = forms.DecimalField(label='賣出', max_digits=9, decimal_places=5, widget=forms.NumberInput(attrs={'class':'form-control'}))
